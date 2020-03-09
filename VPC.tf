5233cdeb3b0457b7b288cf06283d14aa19e2099a
reddy <suchi1686@gmail.com>
2020-03-09 06:14:28 +0000

    adding the AWS resources to the code

--- VPC.tf
+++ VPC.tf
@@ -13,7 +13,33 @@ Name = "dev-link-user-VPC"
 }
 }
 
-# create a public subnets
+# Create the internet gateway
+
+resource "aws_internet_gateway" "Igw" {
+  vpc_id = "${aws_vpc.example.id}"
+
+  tags = {
+    Name = "dev-link-user-igw"
+  }
+}
+
+# create a route table 
+
+
+resource "aws_route_table" "route" {
+  vpc_id = "${aws_vpc.example.id}"
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+
+  tags = {
+    Name = "dev-link-user-routetable"
+  }
+}
+
+# create a public subnets one 
 
 resource "aws_subnet" "publicSubnet1" {
   vpc_id     = "${aws_vpc.example.id}"
@@ -23,3 +49,41 @@ resource "aws_subnet" "publicSubnet1" {
     Name = "dev-link-user-publicsubnet1"
   }
 }
+
+# create a public subnets two 
+
+resource "aws_subnet" "publicSubnet2" {
+  vpc_id     = "${aws_vpc.example.id}"
+  cidr_block = "190.255.1.2/24"
+
+  tags = {
+    Name = "dev-link-user-publicsubnet2"
+  }
+}
+
+
+# create a private subnet one 
+
+resource "aws_subnet" "privateSubnet1" {
+  vpc_id     = "${aws_vpc.example.id}"
+  cidr_block = "190.255.1.3/24"
+
+  tags = {
+    Name = "dev-link-user-privatesubnet1"
+  }
+}
+
+
+# create a private subnets two
+
+resource "aws_subnet" "privateSubnet1" {
+  vpc_id     = "${aws_vpc.example.id}"
+  cidr_block = "190.255.1.4/24"
+
+  tags = {
+    Name = "dev-link-user-privatesubnet2"
+  }
+}
+
+
+
